0e9f0266b9653bab493cf5831b7bc3c88ea4da81
ignat934@gmail.com <ignat934@gmail.com>
2020-11-24 16:04:15 +0300

    first commit

---
+++ src/main/java/main/Controller/ApiPostController.java
@@ -0,0 +1,49 @@
+package main.Controller;
+
+import main.response.responseInfo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiPostController {
+
+
+
+    // GET /api/post/
+
+    // GET /api/post/search/
+
+    // GET /api/post/byTag
+
+    // GET /api/post/moderation
+
+    // GET /api/post/my
+
+    // GET /api/post/{ID}
+
+    // POST /api/post
+
+    // POST /api/image
+
+    // PUT /api/post/{ID}
+
+    // POST /api/comment/
+
+    // GET /api/tag/
+
+    // POST /api/moderation
+
+    // GET /api/calendar/
+
+    // GET /api/statistics/my
+
+    // GET /api/statistics/all
+
+    // POST /api/post/like
+
+    // POST /api/post/dislike
+
+
+
+}
---
+++ src/main/java/main/DefaultController.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String index(Model model)
+    {
+        return "index";
+    }
+}
