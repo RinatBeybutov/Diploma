4156338ddc8160f6db9a2c1fb5593591193de382
ignat934@gmail.com <Tartirotr321!>
2021-04-01 15:18:40 +0300

    v 8.0

---
+++ src/main/java/main/config/Config.java
@@ -0,0 +1,23 @@
+/*
+package main.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class Config implements WebMvcConfigurer {
+
+
+@Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/upload/**")
+        .addResourceLocations("upload/", "classpath:/src/main/java/resources/static/");
+  }
+
+} */
+
